d8e4f870c5aab4f6d85e32f9017a03c82d4a09f9
Danimoz <azubuinedaniel05@gmail.com>
2023-12-07 07:46:42 +0100

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,11 @@ const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata: Metadata = {
   title: 'Bonanza & Sons Mart',
-  description: 'Your One Stop Supermarket in AkwaIbom',
+  description: 'Your One Stop Supermarket in AkwaIbom, Offering Quality Products at Affordable Prices, Official Website of Bonanza & Sons Mart',
+  verification: {
+    google: 'UdrzRq61TgFaNobwzyd9a-2UePaxuk6ADK-GjwaXvz8'
+  },
+  keywords: 'Official Website of Bonanza & Sons Mart, Bonanza & Sons Mart, Bonanza and Sons Mart, Bonanza and Sons, Bonanza and Sons Mart, Bonanza and Sons Mart Uyo, Bonanza and Sons Mart Uyo AkwaIbom, Bonanza and Sons Mart Uyo AkwaIbom State, Bonanza and Sons Mart Uyo AkwaIbom State Nigeria, Bonanza and Sons Mart Uyo'  
 }
 
 export default function RootLayout({
